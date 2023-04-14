b4d015e9a3ec6e6ad11ed195230aa31f9094e0fd
Mats <mats@jawlock.com>
2023-04-14 16:30:48 +0200

    new branch, simple Api

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"net/http"
 
-	"math/rand"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,35 +11,32 @@ type PageView struct {
 	Rubrik string
 }
 
-var theRandom *rand.Rand
-
 func start(c *gin.Context) {
-	c.HTML(http.StatusOK, "home.html", &PageView{Title: "Testar", Rubrik: "Hej Golang, please work"})
+	c.HTML(http.StatusOK, "home.html", &PageView{Title: "Jag testar", Rubrik: "Hej Golang, please work"})
 }
 
 // HTML
 // JSON
 
+var player = struct {
+	Name string `json:"name"`
+	City string `json:"city"`
+}{
+	Name: "Wayne Rooney",
+	City: "Manchester",
+}
+
 func playerJson(c *gin.Context) {
-	var player = struct {
-		Name string `json:"name"`
-		City string `json:"city"`
-	}{
-		Name: "Wayne Rooney",
-		City: "Manchester",
-	}
 
 	c.JSON(http.StatusOK, player)
 }
-
-var config Config
 
 func main() {
 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/**")
 	router.GET("/", start)
-	router.GET("/api/matsgudmundsson", playerJson)
+	router.GET("/api/mittnamn", playerJson)
 	router.Run(":8080")
 
 }
